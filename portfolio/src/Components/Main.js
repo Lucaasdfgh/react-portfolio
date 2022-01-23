2c5a52cd67d08cecb639e784135e26a4576cb664
Lucaasdfgh <lucaascervantes9@gmail.com>
2022-01-23 17:23:59 -0600

    added link

--- portfolio/src/Components/Main.js
+++ portfolio/src/Components/Main.js
@@ -10,13 +10,13 @@ export default function Main() {
                     <img className="profile-img" src="https://i.ibb.co/4mLtqM4/foto-perfil.jpg"></img>
                 </div>
                 <div className="right-home-1">
-                    <h5 className="home-text-1">Front-End Engineer</h5>
-                    <p className="info-home-1">Hi! I'm Lucas a Front-End Engineer from Mexico. </p>
-                    <p className="info-home-1">Check out my projects !</p>
+                    <h5 className="home-text-1">Front-End Developer</h5>
+                    <p className="info-home-1">Hi! I'm Lucas a Front-End Developer-(Engineer in progress) from Mexico. </p>
+                    <p className="info-home-1"><a className="info-home-1" href="/Projects">Check out my projects !</a>  </p> 
                     <div className="info-home icons">
-                        <a target="_blank" href="https://github.com/Lucaasdfgh"><i className="fab fa-github fa-2x"></i> </a> 
-                        <a target="_blank" href="https://www.linkedin.com/in/lucascervantes/"><i className="fab fa-linkedin fa-2x"></i></a>
-                        <a target="_blank" href="https://github.com/Lucaasdfgh"><i className="fab fa-twitter-square fa-2x"></i></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/Lucaasdfgh"><i className="fab fa-github fa-2x"></i> </a> 
+                        <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/lucascervantes/"><i className="fab fa-linkedin fa-2x"></i></a>
+                        <a target="_blank" rel="noreferrer" href="https://github.com/Lucaasdfgh"><i className="fab fa-twitter-square fa-2x"></i></a>
                     </div>              
                 </div>
                 
--- portfolio/src/Components/Navbar.js
+++ portfolio/src/Components/Navbar.js
@@ -1,31 +1,31 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+
 
 export default function Navbar() {
-    return (
-        <div>
-            <nav id="nav-bar-content" class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="/index.html">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
-                <li class="nav-item active">
-                  <a class="nav-link" href="/">About <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Features</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Pricing</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
+  return (
+    <div>
+      <nav id="nav-bar-content" className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/index.html">Home</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+            <a className="nav-link">Projects<Link className="link" to="/invoices"></Link></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link">Experience<Link className="link" to="/invoices"></Link></a>
+            </li>
+          <li className="nav-item active">
+          <a className="nav-link">Invoices<Link className="link" to="/invoices"></Link></a>
+          </li>
+          
               </ul>
             </div>
           </nav>
-        </div>
+        </div >
             
         
     )
